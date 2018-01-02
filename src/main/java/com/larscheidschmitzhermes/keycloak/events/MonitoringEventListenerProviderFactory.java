ad301e6ef6aa23e2d2b2d94f8d3b3b64b2a0584b
Aiden Keating <akeating@redhat.com>
2018-01-02 14:51:23 +0000

    Include debug statements for events directory selection
    Currently there are no debug statements around which events directory
    has been selected. This change includes a debug statement for whether
    the events directory is taken from a config file or from the value of
    an environment variable and also provides the value.

--- src/main/java/com/larscheidschmitzhermes/keycloak/events/MonitoringEventListenerProviderFactory.java
+++ src/main/java/com/larscheidschmitzhermes/keycloak/events/MonitoringEventListenerProviderFactory.java
@@ -1,5 +1,6 @@
 package com.larscheidschmitzhermes.keycloak.events;
 
+import org.jboss.logging.Logger;
 import org.keycloak.Config;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventListenerProviderFactory;
@@ -7,6 +8,7 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 
 public class MonitoringEventListenerProviderFactory implements EventListenerProviderFactory {
+    private static final Logger logger = Logger.getLogger(MonitoringEventListenerProviderFactory.class);
     private final String EVENTS_DIRECTORY_ENV = "KEYCLOAK_PROMETHEUS_EVENTS_DIR";
 
     private String eventsDirectory;
@@ -20,9 +22,12 @@ public class MonitoringEventListenerProviderFactory implements EventListenerProv
     public void init(Config.Scope config) {
         String eventsDirectoryConfigVal = config.get("eventsDirectory");
         if (eventsDirectoryConfigVal != null && !eventsDirectoryConfigVal.isEmpty()) {
+            logger.debug("Using events directory from configuration file: " + eventsDirectoryConfigVal);
             this.eventsDirectory = eventsDirectoryConfigVal;
         } else {
-            this.eventsDirectory = System.getenv(EVENTS_DIRECTORY_ENV);
+            String eventsDirectoryEnvVal = System.getenv(EVENTS_DIRECTORY_ENV);
+            logger.debug("Using events directory from " + EVENTS_DIRECTORY_ENV + " environment variable: " + eventsDirectoryEnvVal);
+            this.eventsDirectory = eventsDirectoryEnvVal;
         }
     }
 
